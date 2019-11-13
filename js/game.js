d0c26270a15adf27738740ee0016bbd1a313867d
unknown <79194954002@yandex.ru>
2019-11-13 22:39:51 +0500

    third commit

--- js/game.js
+++ js/game.js
@@ -1,4 +1,3 @@
-
 let cvs = document.getElementById("canvas");
 let ctx = cvs.getContext("2d");
 
@@ -10,21 +9,56 @@ let pipeDown = new Image(); // Создание объекта нижняя тр
 let land = new Image(); // Создание объекта земля 
 
 //Ниже создаем ссылки на нужные изображения
-bird.src = "img/bird.png"; 
-background.src = "img/background.png"; 
+bird.src = "img/bird.png";
+background.src = "img/background.png";
 pipeUp.src = "img/pipeUp.png";
 pipeDown.src = "img/pipeDown.png";
 land.src = "img/land.png.";
 
-let gap = 90;
 
-function draw() {//Отрисовываем изображения
-	ctx.drawImage(background, 0, 0);
-	ctx.drawImage(pipeUp, 100, 0);
-	ctx.drawImage(pipeDown, 100, 0 + pipeUp.height + gap);
-	ctx.drawImage(land, 0, cvs.height - land.height);
-	ctx.drawImage(bird, 20, 150);
+const moveUp = () => {
+    yPos -= 20;
+}
+
+document.addEventListener("keydown", moveUp); //При нажатии на любую клавишу поднимаем птицу
+
+let pipe = [];
+
+pipe[0] = {
+    x: cvs.width,
+    y: 0
+}
+
+let gap = 90 //Ширина щели между трубами
+
+//Позиция птицы
+let xPos = 10;
+let yPos = 150;
+
+let grav = 1; //Гравитация
+
+const draw = () => { //Отрисовываем изображения
+    ctx.drawImage(background, 0, 0);
+
+    for (let i = 0; i < pipe.length; i++) {
+        ctx.drawImage(pipeUp, pipe[i].x, pipe[i].y);
+        ctx.drawImage(pipeDown, pipe[i].x, pipe[i].y + pipeUp.height + gap);
+
+        pipe[i].x--;
+
+        if (pipe[i].x == 125){
+        	pipe.push({
+        		x: cvs.width,
+        		y: Math.floor(Math.random() * pipeUp.height) - pipeUp.height
+        	});
+        }
+    }
+
+    ctx.drawImage(land, 0, cvs.height - land.height);
+    ctx.drawImage(bird, xPos, yPos);
+
+    yPos += grav;
+    requestAnimationFrame(draw);
 };
 
 land.onload = draw;
-
